6ffe90e7d4ed0dccbce989318eda749d22a7e617
PROJETO\bruno.romero <bruno.romero@dellead.com>
2017-11-16 17:09:21 -0300

    navbar improvements

--- app/assets/javascripts/controllers/sessionsController.js
+++ app/assets/javascripts/controllers/sessionsController.js
@@ -1,3 +1,18 @@
-angular
-  .module("app")
-  .controller("SessionsController", ["$scope", function($scope) {}]);
+angular.module("app").controller("SessionsController", [
+  "$scope",
+  "$location",
+  function($scope, $location) {
+    $scope.login = {};
+    $scope.register = {};
+
+    $scope.log = function(e) {
+      e.preventDefault();
+      $location.path("/");
+    };
+
+    $scope.register = function(e) {
+      e.preventDefault();
+      $location.path("/");
+    };
+  }
+]);
